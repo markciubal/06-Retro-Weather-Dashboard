f3cd14b9bd078065a453fa3658c567e8647151e0
markciubal <mark.ciubal@gmail.com>
2023-04-22 12:05:21 -0700

    Added localStorage for html elements of weather data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,96 @@
 const apiKey = '56cc8773d84f312c66cf68f01cfe031b';
 const nasaKey = '1Gmq9IgfdmLpDj35vHxqVvtjlq6u7jbXo0tpA8jg';
-
+let zuluTime = $('zulu-time');
 let locationOptions = $('#location-options');
+let locationPrevious = $('#location-previous');
 
-$(function () {
-    function kelvinToFahrenheit(kelvin) {
-        let fahrenheit = 1.8 * (kelvin - 273) + 32;
-        return fahrenheit;
-    }
+let locations = [];
 
-    function buildForecast(forecast) {
-        console.log(forecast);
-        for (let i = 0; i < forecast.list.length; i++) {
-            console.log(forecast.list[i]);
-            $('#five-day').append(`<h1>${dayjs(forecast.list[i].dt_txt)}</h1>`);
-            $('#five-day').append(`<h2>${kelvinToFahrenheit(forecast.list[i].main.temp).toFixed(2)}F</h2>`);
-            $('#five-day').append(`<h1>${forecast.list[i].weather[0].main}</h1>`);
-        }
+function storeData(name, forecast, data) {
+    let newLocation = {
+        name: name,
+        forecast: forecast,
+        data: data
     }
-    function displayOptions(options) {
-        let responseHTML = '';
-        for (let i = 0; i < options.length; i++) {
-            responseHTML += `<li><a class="dropdown-item" href="#">${options[i].name}, ${options[i].state}, ${options[i].country}</a></li>`;
-        }
-        locationOptions.html(`
+    locations.push(newLocation);
+    localStorage.setItem('locations', JSON.stringify(locations));
+    updateLocations();
+}
+
+function updateLocations() {
+    let locations = JSON.parse(localStorage.getItem('locations'));
+    for (let i = 0; i < locations.length; i++) {
+        locationPrevious.html(locationPrevious.html() + `<button class="m-2 w-100">${locations[i].name}</button>`)
+    }
+    
+}
+function kelvinToFahrenheit(kelvin) {
+    let fahrenheit = 1.8 * (kelvin - 273) + 32;
+    return fahrenheit;
+}
+
+function callLatLong(text, latitude, longitude) {
+    let apiURL = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=${apiKey}`;
+    fetch(apiURL, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(response => {
+            buildForecast(text, response);
+    })
+}
+
+function buildForecast(text, forecast) {
+    console.log(forecast);
+    let outputHTML = `<div class="col p-5 d-flex align-items-start">
+                        <div class="icon-square rounded text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3" style="background-color: white" >
+                        <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"><br/>
+  
+                        </div>
+                        <div>
+                        <h5 class="fs-2">${text}</h5>
+                        <hr/>
+                        <p>${forecast.weather[0].description.toUpperCase()}</p>
+                        <p>High: ${kelvinToFahrenheit(forecast.main.temp_max).toFixed(2)}° F | Low: ${kelvinToFahrenheit(forecast.main.temp_min).toFixed(2)}° F</p>
+                        <p>Right Now: ${kelvinToFahrenheit(forecast.main.temp).toFixed(2)}° F | Humidity: ${forecast.main.humidity}%</p>
+                        <a href="#" class="btn btn-primary">
+                            Primary button
+                        </a>
+                        </div>
+                    </div>`;
+    $('#five-day').html(outputHTML);
+    storeData(text, forecast, outputHTML);
+}
+
+function displayOptions(options) {
+    let responseHTML = '';
+    for (let i = 0; i < options.length; i++) {
+        responseHTML += `<li><a class="dropdown-item" onclick="callLatLong('${options[i].name}, ${options[i].state}, ${options[i].country}',${options[i].lat},${options[i].lon}); return false;">${options[i].name}, ${options[i].state}, ${options[i].country}</a></li>`;
+    }
+    locationOptions.html(`
+    <div class="m-2 text-center">
         <p>There were a few results for "${options[0].name}".</p>
         <div class="dropdown">
             <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown button
+                ${options.length} Options
             </button>
             <ul class="dropdown-menu">
                 ${responseHTML}
             </ul>
         </div>
-        `);
-    }
-    function callLatLong(latitude, longitude) {
-        let apiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${apiKey}`;
+    </div>`);
+}
+
+function callWeatherFor(event) {
+    event.preventDefault();
+    let location = $('#location').val();
+    let apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`;
+    console.log(apiURL);
+    if (location !== '')
+    {
         fetch(apiURL, {
             method: 'GET',
             headers: {
@@ -45,33 +99,23 @@ $(function () {
         })
         .then(response => response.json())
         .then(response => {
+            console.log(response);
+            if (response.length > 1) {
                 console.log(response);
-                buildForecast(response);
+                displayOptions(response);
+                
+            }
+            console.log(response);
+            console.log(locationOptions.innerHTML);
         })
     }
+}
+window.setInterval(function() {
+    $("#zulu-time").html(dayjs().format('dddd, MMMM DD, YYYY HH:mm:ss '));
+}, 1000)
 
-    function callWeatherFor(event) {
-        event.preventDefault();
-        let location = $('#location').val();
-        let apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`;
-        console.log(apiURL);
-        if (location !== '')
-        {
-            fetch(apiURL, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                },
-            })
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                if (response.length > 1) {
-                    displayOptions(response);
-                }
-                console.log(locationOptions.innerHTML);
-            })
-        }
-    }
+$(function () {
     $('#submit').on('click', callWeatherFor);
+    updateLocations();
+ 
 });
